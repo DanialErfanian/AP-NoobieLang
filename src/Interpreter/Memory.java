a85455b4f70909161f9268d4cfc7cbf50a257866
DanialErfanian <erfaniiyanerfan@gmail.com>
2018-11-04 10:42:48 +0330

    create Memory class

---
+++ src/Interpreter/Memory.java
@@ -0,0 +1,17 @@
+package Interpreter;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Memory {
+    private Map<String, Variable> memory = new TreeMap<>();
+    private Map<String, Integer> lifeLine = new TreeMap<>();
+
+    public Variable get(String name, int line) {
+        return null;// TODO
+    }
+
+    public void add(String name, int lifeLine, Variable variable) {
+        // TODO
+    }
+}
